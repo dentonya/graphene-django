37ae978a4c7ea12d20dc20622c40d950b315711c
dentonya <denatonya@gmail.com>
2024-11-26 11:18:12 +0300

    feat: Added Graphql Mutations to Links app.

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -8,4 +8,13 @@ class Query(links.schema.Query,graphene.ObjectType):
     It inherits from the existing query class in the links schema and extends it with additional functionality as needed.
     """
     pass
-schema = graphene.Schema(query=Query)
+
+class Mutation(links.schema.Mutation, graphene.ObjectType):
+    """Defines the root mutation for the GraphQL schema.
+
+    This class serves as the entry point for all GraphQL mutations in the application. 
+    It inherits from the existing mutation class in the links schema and extends it with additional functionality as needed.
+    """
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -32,5 +32,64 @@ class Query(graphene.ObjectType):
             QuerySet: A queryset containing all Link objects.
         """
         return Links.objects.all()
+    
+class CreateLink(graphene.Mutation):
+    """
+    CreateLink is a GraphQL mutation for creating a new link entry. 
+    It encapsulates the necessary fields for a link, including its identifier, URL, and description.
+
+    Attributes:
+        id (int): The unique identifier for the link.
+        url (str): The URL of the link.
+        description (str): A brief description of the link.
+    """
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    class Arguments:
+        """
+        Arguments is a class that defines the input parameters for the CreateLink mutation. 
+        It includes the URL and description of the link to be created.
+        Data you send to the server.
+
+        Attributes:
+            url (str): The URL of the link.
+            description (str): A brief description of the link.
+
+        Methods:
+            mutate(info, url, description):
+                Creates a new link with the provided URL and description, 
+                saving it to the database and returning the created link's details.
+            
+            Args:
+                info: The GraphQL context information.
+                url (str): The URL of the link to create.
+                description (str): A brief description of the link to create.
+
+            Returns:
+                CreateLink: An instance containing the details of the created link.
+        """
+        url = graphene.String()
+        description = graphene.String()
+        
+    def mutate(self,info,url,description):
+        link = Links(url=url,description=description)
+        link.save()
+        
+        return CreateLink(
+            id=link.id, 
+            url=link.url, 
+            description=link.description
+        )
+            
+class Mutation(graphene.ObjectType):
+    """
+    Mutation root type.
+
+    This class defines the root mutation type for the GraphQL API. 
+    It includes a single field, 'create_link', which creates a new link.
+    """
+    create_link = CreateLink.Field()
 
 
